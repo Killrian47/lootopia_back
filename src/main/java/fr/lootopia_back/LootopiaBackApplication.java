f96049e74ff73412dec311e417fe692fbb9caf4e
Killian Portier <killian.portier@yahoo.fr>
2025-03-20 07:46:45 +0100

    Init the project with Neon postgreSQL DB

---
+++ src/main/java/fr/lootopia_back/LootopiaBackApplication.java
@@ -0,0 +1,13 @@
+package fr.lootopia_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LootopiaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LootopiaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/lootopia_back/LootopiaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lootopia_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LootopiaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
