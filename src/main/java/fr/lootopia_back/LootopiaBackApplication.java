c328839a83d41fdb31180093a5ab0bb6bd5149ed
Killian Portier <killian.portier@yahoo.fr>
2025-03-20 10:58:01 +0100

    feat: Add BCrypt in UserService

--- src/main/java/fr/lootopia_back/LootopiaBackApplication.java
+++ src/main/java/fr/lootopia_back/LootopiaBackApplication.java
@@ -2,18 +2,11 @@ package fr.lootopia_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class LootopiaBackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LootopiaBackApplication.class, args);
-	}
-
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 }
