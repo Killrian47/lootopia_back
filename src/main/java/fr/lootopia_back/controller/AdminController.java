9756914b6f129059a4df8ff773c1e4093ec2ddc5
Killian Portier <killian.portier@yahoo.fr>
2025-04-08 14:24:11 +0200

    fix: corrections after review

--- src/main/java/fr/lootopia_back/controller/AdminController.java
+++ src/main/java/fr/lootopia_back/controller/AdminController.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.lootopia_back.model.User;
 import fr.lootopia_back.service.UserService;
-import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -26,8 +27,8 @@ public class AdminController {
   }
 
   // Endpoint pour supprimer un utilisateur
-  @RequestMapping("/users/delete/{id}")
-  public String deleteUser(@PathParam("id") Long id) {
+  @DeleteMapping("/users/delete/{id}")
+  public String deleteUser(@PathVariable Long id) {
     userService.deleteUser(id);
     return "User with id " + id + " has been deleted";
   }
